dddf6c5857fbbc66da1219d97adade24b54e8103
Bernardo Andrade <you@example.com>
2024-09-24 11:23:33 -0300

    feat:token page layout fix

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default function TokenPage({ params }: { params: { slug: string } }): JSX
         const response = await axios.get(`/api/coins`, {
           params: { tokenAddress: params.slug }
         });
-        setToken(response.data)
+        setToken(response.data);
       } catch (err) {
         if (err instanceof AxiosError || err instanceof Error) {
           setError(err.message);
@@ -53,35 +53,34 @@ export default function TokenPage({ params }: { params: { slug: string } }): JSX
 
   return (
     <div className="relative flex flex-col items-center justify-center max-w-7xl mx-auto pt-10">
-      <div className="grid grid-cols-3 gap-6 w-full">
-        <div className="col-span-2">
-          <div ref={topRef} className="mb-4 flex">
-            <span
-              className="text-sm bg-transparent text-white mb-2 cursor-pointer"
-              onClick={scrollToBottom}
-            >
+      <div className="grid grid-cols-2 gap-6 w-full">
+        {/* Coluna do Gráfico */}
+        <div className="col-span-1 order-1">
+          <div ref={topRef} className="mb-4 flex justify-start">
+            <span className="text-sm bg-transparent text-white mb-2 cursor-pointer" onClick={scrollToBottom}>
               [scroll down]
             </span>
           </div>
           <LightweightChart tokenMint={token.mint} />
-          <div className="col-span-1 flex flex-col items-center">
-          <img src={token.image_uri} alt={token.name} className="w-full h-auto mb-4" />
-          <h1 className="text-xl font-semibold">{token.name} ({token.symbol})</h1>
-          <p className="text-sm">{token.description}</p>
-          <a href={token.website} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+        </div>
+
+        {/* Coluna da Imagem e Informações */}
+        <div className="col-span-1 order-2 flex flex-col items-center">
+          <img src={token.image_uri} alt={token.name} className="w-64 h-auto mb-4 border-8 border-black" />
+          <h1 className="text-xl font-semibold text-center">{token.name} ({token.symbol})</h1>
+          <p className="text-sm text-center">{token.description}</p>
+          <a href={token.website} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline mt-2">
             Visit Website
           </a>
-          <ThreadTradesSection tokenAddress={token.mint} creator={token.creator} />
         </div>
-          <div ref={bottomRef} className="mt-4 flex">
-            <span
-              className="text-sm bg-transparent text-white mt-2 cursor-pointer"
-              onClick={scrollToTop}
-            >
-              [scroll up]
-            </span>
-          </div>
-        </div>
+      </div>
+      <div className="flex w-full justify-center items-center mt-4">
+        <ThreadTradesSection tokenAddress={token.mint} creator={token.creator} />
+      </div>
+      <div ref={bottomRef} className="w-full flex justify-center mt-4">
+        <span className="text-sm bg-transparent text-white cursor-pointer" onClick={scrollToTop}>
+          [scroll up]
+        </span>
       </div>
     </div>
   );
