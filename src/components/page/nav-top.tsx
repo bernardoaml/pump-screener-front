a109dcbe237bba4272380d566e89bed191f90b31
Bernardo Andrade <you@example.com>
2024-10-07 17:09:30 -0300

    feat:sol price component

--- src/components/page/nav-top.tsx
+++ src/components/page/nav-top.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import SolPrice from '../HomeComponents/sol-price';
 
 // import {W3Btn} from '@/wallet/w3-btn';
 // import Notifications from '@/content/Notifications';
@@ -15,6 +16,8 @@ export function NavTop() {
     >
       <div className="flex w-full max-w-7xl justify-center">
         <div className="inline-flex h-24 w-full items-center justify-between p-2 px-6">
+          <div className='flex flex-row items-center'>
+
           <Link href="/" prefetch={false}>
           <Image
               src="/logo.webp"
@@ -22,8 +25,10 @@ export function NavTop() {
               height={90}
               alt="Pump.fun Alternative"
               className="mr-10"
-            />
+              />
           </Link>
+          <SolPrice/>
+          </div>
 
 
           <ul className="flex flex-row mt-8 mb-4">
